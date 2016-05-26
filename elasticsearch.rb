592a187c52f435b1e64afd71f75460de29ded81e
Eric London <eric.london@arcadiasolutions.com>
2016-05-26 14:29:04 -0400

    rubocop

--- elasticsearch.rb
+++ elasticsearch.rb
@@ -7,19 +7,17 @@ require 'elasticsearch_api'
 
 options = {}
 OptionParser.new do |opts|
-
-  opts.on("--delete-index", "Delete index") do |v|
+  opts.on('--delete-index', 'Delete index') do |_v|
     options[:action] = :delete_index
   end
 
-  opts.on("--create-mapping", "Create index mapping") do |v|
+  opts.on('--create-mapping', 'Create index mapping') do |_v|
     options[:action] = :create_mapping
   end
 
-  opts.on("--index", "Index iTunes") do |v|
+  opts.on('--index', 'Index iTunes') do |_v|
     options[:action] = :index_itunes
   end
-
 end.parse!
 
 raise 'Nothing to do!' if options[:action].nil?
